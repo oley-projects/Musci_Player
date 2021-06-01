4fcd0ea9a0133ae5242a8b8817223fb2de49625b
oley-projects <oley.for.git@gmail.com>
2021-06-01 14:38:10 +0300

    player animation, play / pause functionality

---
+++ javascript.js
@@ -0,0 +1,55 @@
+const musicContainer = document.getElementById('music-container'),
+      playBtn = document.getElementById('play'),
+      prewBtn = document.getElementById('prew'),
+      nextBtn = document.getElementById('next'),
+
+      audio = document.getElementById('audio'),
+      progress = document.getElementById('progress'),
+      progressContainer = document.getElementById('progress-container'),
+      title = document.getElementById('title'),
+      cover = document.getElementById('cover'),
+
+      // Song titles
+      songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+// Pause song
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
